1719ee40e9954396425c150a58bb868a64da166b
Halfblood1223 <yashveersemlan@gmail.com>
2020-11-06 10:19:16 -0600

    Adding econ help menu

--- localbebot.py
+++ localbebot.py
@@ -111,16 +111,16 @@ class Mod(commands.Cog):
     
     @commands.has_permissions(manage_guild=True)
     @commands.command()
-    async def welcome(self, ctx, state: str, *, channel):
+    async def welcome(self, ctx, state: str, *, channel=None):
         """Set Up a welcome message in the specified channel."""
         state = state.upper()
         ogchannel = channel
         print(type(channel), channel)
         try:
             channel = discord.utils.get(ctx.guild.channels, name=channel)
-            if channel == None:
+            if channel == None and state == "OFF":
                 channel = ogchannel
-                raise Exception("Channel is None")
+                await ctx.send("You need to set a channel if your trying to turn it on.")
         except Exception as e:
             try:
                 channel = await self.bot.fetch_channel(int(channel[2:len(channel)-1]))
@@ -1052,44 +1052,56 @@ class Help(commands.Cog):
             query = query.upper()
             if query == "MOD":
                 embed = discord.Embed(title="Mod Help Menu", description="All the mod commands Bebot has.", color=0x88B04B)
-                embed.set_thumbnail(url="https://i.pinimg.com/originals/01/2e/71/012e716ba09bab32c9f3ea163b7663af.gif")
-                embed.add_field(name="Clear", value="Clears the specified amount of messages. Defaults to 5.", inline=False)
-                embed.add_field(name="Nuke", value="Clears 1000 messages. Causes major lag. Use at your own risk.", inline=False)
-                embed.add_field(name="Kick", value="Kicks specified member from the server.", inline=False)
-                embed.add_field(name="Ban", value="Bans specified member from the server.", inline=False)
-                embed.add_field(name="Unban", value="Unbans the specified member from the server. Takes in an @mention or an id number. An id is not the 4 digits at the end of a username.", inline=False)
-                embed.add_field(name="Mute", value="Mutes the specified member. Gives them the mute role and text mutes them as well as voice muting them. If they are in a VC they need to leave before voice mute takes effect.", inline=False)
-                embed.add_field(name="Unmute", value="Unmutes the specified member. Takes away the unmuted role. If they are in a VC they need to leave for it to be active.", inline=False)
-                embed.add_field(name="Welcome", value="Sets up a welcome message for new members in a specified channel. Used by typing b.welcome on #textchannel or b.welcome off #textchannel.", inline=False)
+                embed.set_thumbnail(url="https://24.media.tumblr.com/57407804c86056de4a3bae164972261c/tumblr_msroypBO801rjlu67o1_500.gif")
+                embed.add_field(name="Clear", value="Clears the specified amount of messages. Defaults to 5. Summoned using b.clear", inline=False)
+                embed.add_field(name="Nuke", value="Clears 1000 messages. Causes major lag. Use at your own risk. Summoned using b.nuke", inline=False)
+                embed.add_field(name="Kick", value="Kicks specified member from the server. Summoned using b.kick @person", inline=False)
+                embed.add_field(name="Ban", value="Bans specified member from the server. b.ban @person", inline=False)
+                embed.add_field(name="Unban", value="Unbans the specified member from the server. Takes in an @mention or an id number. An id is not the 4 digits at the end of a username. Summoned using b.unban userid", inline=False)
+                embed.add_field(name="Mute", value="Mutes the specified member. Gives them the mute role and text mutes them as well as voice muting them. If they are in a VC they need to leave before voice mute takes effect. Summoned using b.mute @person", inline=False)
+                embed.add_field(name="Unmute", value="Unmutes the specified member. Takes away the unmuted role. If they are in a VC they need to leave for it to be active. Turned on using b.welcome on #channelname and Turned off using b.welcome off #channelname", inline=False)
+                embed.add_field(name="Welcome", value="Sets up a welcome message for new members in a specified channel. Summoned using b.welcome on #textchannel or b.welcome off", inline=False)
                 embed.set_footer(text="b.help <category> returns all the commands in a category. For example b.help mod returns all the Mod commands")
                 await ctx.send(embed=embed)
                 return
             elif query == "MUSIC":
                 embed = discord.Embed(title="Music Help Menu", description="All the Music commands Bebot has.", color=0x88B04B)
                 embed.set_thumbnail(url="https://i.pinimg.com/originals/01/2e/71/012e716ba09bab32c9f3ea163b7663af.gif")
-                embed.add_field(name="Join", value="Joins the users current voice channel.", inline=False)
-                embed.add_field(name="Leave/Stop", value="Leaves the bots current voice channel.", inline=False)
-                embed.add_field(name="Play", value="Play the specified song. Use b.play dont stop believing or b.play video/playlist url", inline=False)
-                embed.add_field(name="Pause", value="Pause the current playing song.", inline=False)
-                embed.add_field(name="Unpause/Resume", value="Resume the current paused song.", inline=False)
-                embed.add_field(name="Queue", value="Show the current queue.", inline=False)
+                embed.add_field(name="Join", value="Joins the users current voice channel. Summoned using b.play", inline=False)
+                embed.add_field(name="Leave/Stop", value="Leaves the bots current voice channel. Summoned using b.leave or b.stop", inline=False)
+                embed.add_field(name="Play", value="Play the specified song. Use b.play dont stop believing or b.play url", inline=False)
+                embed.add_field(name="Pause", value="Pause the current playing song. Summoned using b.pause", inline=False)
+                embed.add_field(name="Unpause/Resume", value="Resume the current paused song. Summoned using b.resume of b.unpause", inline=False)
+                embed.add_field(name="Queue", value="Show the current queue. Summoned using b.queue", inline=False)
                 embed.add_field(name="Volume", value="Set the volume of the current player to a number. IE b.volume 10 sets the volume to 10.")
                 embed.set_footer(text="b.help <category> returns all the commands in a category. For example b.help mod returns all the Mod commands")
                 await ctx.send(embed=embed)
                 return
             elif query == "ANIME":
                 embed = discord.Embed(title="Anime Help Menu", description="All the Anime commands Bebot has.", color=0x88B04B)
-                embed.set_thumbnail(url="https://i.pinimg.com/originals/01/2e/71/012e716ba09bab32c9f3ea163b7663af.gif")
+                embed.set_thumbnail(url="https://i.pinimg.com/originals/3b/31/03/3b3103df897011165b21afd57c3aa7fa.gif")
                 embed.add_field(name="Anisearch", value="Get data of an anime by it's name. Spelling is very finnicky so try spelling it different ways. Capitalize the first letter.", inline=False)
-                embed.add_field(name="Anistream", value="Get a stream of an anime by its name and episode number. Summoned using b.anistream")
+                embed.add_field(name="Anistream", value="Get a stream of an anime by its name and episode number. If the first time doesnt work add tv to the end. Summoned using b.anistream IE b.anistream Black Clover TV")
+                embed.add_field(name="WatchBebop", value="Get an episode of Cowboy Bebop from the google drive archive. Episode 7 isn't available. Summoned using b.watchbebop episodenum IE b.watchbebop 12", inline=False)
                 embed.set_footer(text="b.help <category> returns all the commands in a category. For example b.help mod returns all the Mod commands")
                 await ctx.send(embed=embed)
                 return
             elif query == "GAMING":
                 embed = discord.Embed(title="Gaming Help Menu", description="All the Gaming commands Bebot has.", color=0x88B04B)
-                embed.set_thumbnail(url="https://i.pinimg.com/originals/01/2e/71/012e716ba09bab32c9f3ea163b7663af.gif")
+                embed.set_thumbnail(url="https://i.imgur.com/jEsFoj9.gif")
                 embed.add_field(name="MW", value="Get your modern warfare stats based on platform(xbl, psn, acti) gamertag and gamemode(warzone, mp). IE b.mw xbl Halfblood1223", inline=False)
                 embed.add_field(name="Fortnite", value="Get your Fortnite stats based on platform(pc or console) server region(NAW, NAE, EU) and gamertag IE b.fortnite pc NAE Ninja", inline=False)
+                embed.set_footer(text="b.help <category> returns all the commands in a category. For example b.help mod returns all the Mod commands")
+                await ctx.send(embed=embed)
+                return
+            elif query == "Economy" or query == "Econ":
+                embed = discord.Embed(title="Economy Help Menu", description="All the Economy commands Bebot has.", color=0x88B04B)
+                embed.set_thumbnail(url="https://64.media.tumblr.com/7d3ebbb1f32cf5296d4555a1fc182ac0/cb4a12f342102665-14/s400x600/bf7007b488dfea5a98724210ebfd29829dd9d099.gifv")
+                embed.add_field(name="Daily", value="Get your 5000 woolongs every 24 hours. Summoned using b.daily", inline=False)
+                embed.add_field(name="ToHand", value="Send money from your bank to your hand. Summoned using b.tohand amount.", inline=False)
+                embed.add_field(name="ToBank", value="Send money from your hand to your bank. Summoned using b.tobank amount.", inline=False)
+                embed.add_field(name="Balance/Bal", value="Get the balance of yourself, another player, or the leaderboards. Defaults to you. Summoned using b.bal @person or b.bal or b.bal all", inline=False)
+                embed.add_field(name="BlackJack", value="Play a game of BlackJack for up to double your cash. Summoned using b.bj amount", inline=False)
                 embed.set_footer(text="b.help <category> returns all the commands in a category. For example b.help mod returns all the Mod commands")
                 await ctx.send(embed=embed)
                 return
