8bc0611eff4650ca3b11ba1d0e6dd1e6943f27a1
Halfblood1223 <yashveersemlan@gmail.com>
2020-11-07 02:39:34 -0600

    Updating for use of env variables

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import wavelink
+import os
+from dotenv import load_dotenv
 
 #setup
 intents = discord.Intents.default()
@@ -8,7 +10,10 @@ intents.members = True
 client = commands.Bot(command_prefix="b.", intents=intents, case_insensitive=True)
 client.wavelink = wavelink.Client(bot=client)
 client.remove_command('help')
-
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+TOKEN = os.getenv('APIKEY')
+print(TOKEN)
 
 @client.event
 async def on_ready():
@@ -22,5 +27,5 @@ client.load_extension("cogs.search")
 client.load_extension("cogs.econ")
 client.load_extension("cogs.help")
 client.load_extension("cogs.XP")
-client.run('NzY5NDA5MTMwMzAwNDQwNTk2.X5OmFw.omDEZarxQyWWRY-Y2LPbEWjhi_0')
+client.run(TOKEN)
 
